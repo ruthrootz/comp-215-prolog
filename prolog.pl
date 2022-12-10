357dfd2d2403e2891c58a84060657582c3bb931b
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-12-10 13:53:25 -0500

    Update prolog.pl

--- prolog.pl
+++ prolog.pl
@@ -2,26 +2,34 @@
 % date: 12/7/2022
 % purpose: methods to handle basic array operations
 
+% if a list is empty, just return the result List
 append1([], List, List).
-append1([X | T], List, [X | Y]) :- append1(T, List, Y).
+% grab the head of the list and add it to the result R, then call append1 with the tail
+append1([X | Tail], List, [X | R]) :- append1(Tail, List, R).
 
+% if the search term and the head of the list match, exit
 member1(X, [X | _]).
+% the head doesn't match the search term, so run member1 again
 member1(X, [_ | List]) :- member1(X, List).
 
+% if the search term and the head of the list match, return the tail
 rest_list(X, [X | Tail], Tail).
+% the head doesn't match the search term, so run rest_list again
 rest_list(X, [_ | Tail], Output) :- rest_list(X, Tail, Output).
 
-% case: list is empty, nothing else to do
+% list is empty, nothing else to do
 flatten1([], []).
-% case: list head is itself a list
-flatten1([[X | T1] | T2], R) :- flatten1([X | [T1 | T2]], R).
-% case: list head is an empty list
-flatten1([[] | T], R) :- flatten1(T, R).
-% case: list head is a non-list element
+% list head is itself a list
+flatten1([[X | Tail1] | Tail2], R) :- flatten1([X | [Tail1 | Tail2]], R).
+% list head is an empty list
+flatten1([[] | Tail], R) :- flatten1(Tail, R).
+flatten1([[] | Tail], R) :- flatten1(Tail, R).
+% list head is a non-list element
 	% take X off list, add it to R, then call flatten1 again
-flatten1([X | T1], [X | R]) :- flatten1(T1, R).
+flatten1([X | Tail], [X | R]) :- flatten1(Tail, R).
 
+% 
 zip([], [], []).
 zip([], _, []).
 zip(_, [], []).
-zip([X | T1], [Y | T2], [ X-Y | R]) :- zip(T1, T2, R).
+zip([X | Tail1], [Y | Tail2], [ X-Y | R]) :- zip(Tail1, Tail2, R).
